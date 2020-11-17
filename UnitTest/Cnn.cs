7e852ef0cd9ba87cf3836bff67f13138c86bcd55
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-17 12:12:39 -0300

    inprove tests with cnn

---
+++ UnitTest/Cnn.cs
@@ -0,0 +1,15 @@
+﻿using API.Connection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace UnitTest
+{
+  public static class Cnn
+  {
+    public static IDbCnn GetCnn(ILoggerFactory loggerFactory)
+    {
+      var config = new ConfigurationBuilder().AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true).Build();
+      return new PedidoSqlCnn(loggerFactory?.CreateLogger<PedidoSqlCnn>(), config);
+    }
+  }
+}
