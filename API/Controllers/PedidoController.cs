f0b801e09a44140c26d67b6391d93d1c71b1c1d1
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-17 18:07:49 -0300

    add controllers pedido actions

--- API/Controllers/PedidoController.cs
+++ API/Controllers/PedidoController.cs
@@ -1,9 +1,9 @@
-﻿using API.Model;
-using API.Repository;
+﻿using API.Service;
+using API.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
@@ -11,25 +11,29 @@ namespace API.Controllers
   [Route("[controller]")]
   public class PedidoController : ControllerBase
   {
-    private static readonly string[] Summaries = new[]
-    {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
     private readonly ILogger<PedidoController> _logger;
-    private readonly IPedidoRepository _pedidoRepository;
+    private readonly IPedidoService _pedidoService;
 
-    public PedidoController(ILogger<PedidoController> logger, IPedidoRepository pedidoRepository)
+    public PedidoController(ILogger<PedidoController> logger, IPedidoService pedidoService)
     {
       _logger = logger;
 
-      _pedidoRepository = pedidoRepository;
+      _pedidoService = pedidoService;
     }
 
     [HttpGet]
-    public IEnumerable<Pedido> Get()
-    {
-      return _pedidoRepository.GetAll();
-    }
+    public async Task<IEnumerable<PedidoViewModel>> GetAll() => await _pedidoService.GetAll().ConfigureAwait(false);
+
+    [HttpGet]
+    public async Task<PedidoViewModel> Get(int id) => await _pedidoService.Get(id).ConfigureAwait(false);
+
+    [HttpPost]
+    public async Task<bool> Post(PedidoViewModel pedido) => await _pedidoService.InsertUpdate(pedido).ConfigureAwait(false);
+
+    [HttpPut]
+    public async Task<bool> Put(PedidoViewModel pedido) => await _pedidoService.InsertUpdate(pedido).ConfigureAwait(false);
+
+    [HttpDelete]
+    public async Task<bool> Delete(int id) => await _pedidoService.Delete(id).ConfigureAwait(false);
   }
 }
