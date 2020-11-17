067865f9867910e4533fda0bd1789e5f1b25510f
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-17 10:14:35 -0300

    add pedido and item structure

---
+++ API/Model/Pedido.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace API.Model
+{
+  public class Pedido
+  {
+    /// <summary>
+    /// Número do Pedido
+    /// </summary>
+    [JsonPropertyName("pedido")]
+    public int Numero { get; set; }
+
+    /// <summary>
+    /// Itens do Pedido
+    /// </summary>
+    [JsonPropertyName("itens")]
+    public List<Item> Itens { get; set; }
+  }
+}
